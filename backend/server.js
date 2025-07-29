3eb1bfd03b6a3d7cf3f298e99f3c9f1a3d79b851
Shashank Shekhar <56396662+shashank0000000@users.noreply.github.com>
2025-07-29 13:39:38 +0530

    changed port

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app = express()
-const port = 4020
+const port = process.env.PORT || 4020
 
 //middleware
 app.use(express.json())
