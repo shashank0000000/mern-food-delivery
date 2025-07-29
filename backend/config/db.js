b219c59f6c5db281239effed0edf95369d16c4db
Shashank Shekhar <56396662+shashank0000000@users.noreply.github.com>
2025-07-29 15:11:19 +0530

    pass change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://shashankshekhar01776:Sha%401234@cluster0.wahwlcf.mongodb.net/videotube?retryWrites=true&w=majority').then(() => console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://shashankshekhar01776:Sha131113@cluster0.wahwlcf.mongodb.net/videotube?retryWrites=true&w=majority').then(() => console.log("DB Connected"));
 }
